bbc32c0f74d9f3b56a28e3d04742c854b1df8fc0
hqq <qianqian6755@sina.com>
2019-07-10 16:25:02 +0800

    First commit all.

---
+++ src/com/hqq/git/input/HelloGit.java
@@ -0,0 +1,12 @@
+package com.hqq.git.input;
+
+/**
+ * Author: qq_huang
+ * Date: 2019/7/10
+ * Time: 16:20
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
