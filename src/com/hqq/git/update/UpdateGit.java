329d952dfe1e72d7fcbd960ed203464540ee980a
hqq <qianqian6755@sina.com>
2019-07-10 16:53:22 +0800

    Test update!

---
+++ src/com/hqq/git/update/UpdateGit.java
@@ -0,0 +1,12 @@
+package com.hqq.git.update;
+
+/**
+ * Author: qq_huang
+ * Date: 2019/7/10
+ * Time: 16:51
+ */
+public class UpdateGit {
+    public static void main(String[] args) {
+        System.out.println("I update this project!");
+    }
+}
